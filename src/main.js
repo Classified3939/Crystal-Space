794e37afdaec91341e1d7727dd756d8e58bd0ee1
Classified3939 <classified390@gmail.com>
2023-02-20 06:23:42 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import "floating-vue/dist/style.css";
 import App from "./App.vue";
-import { GameController } from "@./scripts/GameController";
+import { GameController } from "./scripts/GameController";
 import { AllItems } from "./scripts/items/allItems";
 import FloatingVue from "floating-vue";
 
